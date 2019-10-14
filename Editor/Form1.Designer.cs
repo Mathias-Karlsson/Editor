7605248cfaaaec19aa04a7884374a2ea8c1be379
mathkarl <mathkarl@LSE-B2725>
2019-10-14 11:15:16 +0200

    Blev klar med Editor och fixade problemet med öppna

--- Editor/Form1.Designer.cs
+++ Editor/Form1.Designer.cs
@@ -28,62 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.file = new System.Windows.Forms.ToolStripMenuItem();
-            this.extra = new System.Windows.Forms.ToolStripMenuItem();
-            this.open = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveAs = new System.Windows.Forms.ToolStripMenuItem();
-            this.close = new System.Windows.Forms.ToolStripMenuItem();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.file = new System.Windows.Forms.ToolStripMenuItem();
+            this.open = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.close = new System.Windows.Forms.ToolStripMenuItem();
+            this.extra = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.file,
-            this.extra});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(750, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // file
-            // 
-            this.file.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.open,
-            this.saveAs,
-            this.close});
-            this.file.Name = "file";
-            this.file.Size = new System.Drawing.Size(46, 20);
-            this.file.Text = "Arkiv";
-            // 
-            // extra
-            // 
-            this.extra.Name = "extra";
-            this.extra.Size = new System.Drawing.Size(44, 20);
-            this.extra.Text = "Extra";
-            // 
-            // open
-            // 
-            this.open.Name = "open";
-            this.open.Size = new System.Drawing.Size(180, 22);
-            this.open.Text = "Öppna";
-            // 
-            // saveAs
-            // 
-            this.saveAs.Name = "saveAs";
-            this.saveAs.Size = new System.Drawing.Size(180, 22);
-            this.saveAs.Text = "Spara som";
-            // 
-            // close
-            // 
-            this.close.Name = "close";
-            this.close.Size = new System.Drawing.Size(180, 22);
-            this.close.Text = "Stäng";
             // 
             // richTextBox1
             // 
@@ -97,6 +52,55 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.file,
+            this.extra});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(750, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked_1);
+            // 
+            // file
+            // 
+            this.file.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.open,
+            this.saveAs,
+            this.close});
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(46, 20);
+            this.file.Text = "Arkiv";
+            // 
+            // open
+            // 
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(180, 22);
+            this.open.Text = "Öppna";
+            this.open.Click += new System.EventHandler(this.open_Click_1);
+            // 
+            // saveAs
+            // 
+            this.saveAs.Name = "saveAs";
+            this.saveAs.Size = new System.Drawing.Size(180, 22);
+            this.saveAs.Text = "Spara som";
+            this.saveAs.Click += new System.EventHandler(this.saveAs_Click);
+            // 
+            // close
+            // 
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(180, 22);
+            this.close.Text = "Stäng av";
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // extra
+            // 
+            this.extra.Name = "extra";
+            this.extra.Size = new System.Drawing.Size(44, 20);
+            this.extra.Text = "Extra";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -104,10 +108,8 @@
             this.ClientSize = new System.Drawing.Size(750, 331);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Editor";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -116,16 +118,15 @@
         }
 
         #endregion
-
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem file;
         private System.Windows.Forms.ToolStripMenuItem open;
         private System.Windows.Forms.ToolStripMenuItem saveAs;
         private System.Windows.Forms.ToolStripMenuItem close;
         private System.Windows.Forms.ToolStripMenuItem extra;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- Editor/Form1.cs
+++ Editor/Form1.cs
@@ -17,13 +17,38 @@ namespace Editor
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void open_Click_1(object sender, EventArgs e)
         {
             DialogResult dialogResult = openFileDialog1.ShowDialog();
             if (dialogResult == DialogResult.OK)
             {
                 richTextBox1.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
             }
+
+        }
+
+        private void saveAs_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = saveFileDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+            }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
