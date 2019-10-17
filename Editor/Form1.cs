5b88a5126dd99de6ee6aef951df0e79160689524
mathkarl <mathkarl@LSE-B2725>
2019-10-17 11:13:07 +0200

    Har lagt in kommentarer i Editor beskriver vad varje _Click gör

--- Editor/Form1.cs
+++ Editor/Form1.cs
@@ -27,6 +27,10 @@ namespace Editor
 
         }
 
+        /// <summary>
+        /// open_Click_1 är kopplad till Öppna i Arkiv och användingen är att öppna en txt fil till programmet
+        /// </summary>
+        /// <param name="args"></param>
         private void open_Click_1(object sender, EventArgs e)
         {
             DialogResult dialogResult = openFileDialog1.ShowDialog();
@@ -37,6 +41,10 @@ namespace Editor
 
         }
 
+        /// <summary>
+        /// saveAs_Click är kopplad till Spara som i Arkiv och den används till att spara vad du har skrivit till en txt fil
+        /// </summary>
+        /// <param name="args"></param>
         private void saveAs_Click(object sender, EventArgs e)
         {
             DialogResult dialogResult = saveFileDialog1.ShowDialog();
@@ -46,6 +54,10 @@ namespace Editor
             }
         }
 
+        /// <summary>
+        /// close_Click är kopplad till Stäng i Arkiv och används till att stänga ner Editor när man är klar med programmet
+        /// </summary>
+        /// <param name="args"></param>
         private void close_Click(object sender, EventArgs e)
         {
             Application.Exit();
